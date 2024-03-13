11877c0cad930b94a38bea7ee96431eadbef15cf
Yuvraj17 <yuvibabariya001@gmail.com>
2024-03-13 12:56:23 +0530

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ import 'core/themes/app_theme.dart';
 
 class MyHttpOverrides extends HttpOverrides {
   @override
-  HttpClient createHttpClient(SecurityContext? context) {
+  HttpClient createHttpClient(SecurityContext? context) { 
     return super.createHttpClient(context)
       ..badCertificateCallback =
           (X509Certificate cert, String host, int port) => true;
